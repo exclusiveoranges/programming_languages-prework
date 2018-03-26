adfe73c0eec60c68415e107be1173024033f25a7
Ryan O'Toole <otoolerj@gmail.com>
2018-03-26 23:59:05 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,12 +5,12 @@ def reformat_languages(languages)
   h1 = languages[:oo]
   h2 = languages[:functional]
   new_hash = h1.merge(h2)
-  new_hash[:ruby][:style] = "[:oo]"
-  new_hash[:javascript][:style] = "[:oo, :functional]"
-  new_hash[:python][:style] = "[:oo]"
-  new_hash[:java][:style] = "[:oo]"
-  new_hash[:clojure][:style] = "[:functional]"
-  new_hash[:erlang][:style] = "[:functional]"
-  new_hash[:scala][:style] = "[:functional]"
+  new_hash[:ruby][:style] = [:oo]
+  new_hash[:javascript][:style] = [:oo, :functional]
+  new_hash[:python][:style] = [:oo]
+  new_hash[:java][:style] = [:oo]
+  new_hash[:clojure][:style] = [:functional]
+  new_hash[:erlang][:style] = [:functional]
+  new_hash[:scala][:style] = [:functional]
   new_hash
 end
