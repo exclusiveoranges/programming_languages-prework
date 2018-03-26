9128fed631d31ffa5befb922ccbdc40e4b022b18
Ryan O'Toole <otoolerj@gmail.com>
2018-03-26 23:57:05 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -12,4 +12,5 @@ def reformat_languages(languages)
   new_hash[:clojure][:style] = "[:functional]"
   new_hash[:erlang][:style] = "[:functional]"
   new_hash[:scala][:style] = "[:functional]"
+  new_hash
 end
